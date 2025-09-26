1802174e086f03cd3f6582773fd3c8529db96921
Erica Cau <e.cau@studenti.unipi.it>
2025-09-26 09:52:38 +0200

    fix legend area chart

--- llm_network/viz/area_chart.py
+++ llm_network/viz/area_chart.py
@@ -105,7 +105,7 @@ class StackedAreaChart:
         ax.set_ylim(0, 1)  # Exact bounds, no padding
         
         ax.set_ylabel("")
-        ax.legend([])
+        ax.legend().set_visible(False)
         
         # Remove ALL margins and padding
         ax.margins(0)
