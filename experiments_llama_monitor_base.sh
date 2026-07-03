d53579e23d5588aa9f207d3d00b266c4fb801fca
Erica Cau <erica@eduroam-54-147.pi.cnr.it>
2026-07-03 13:21:31 +0200

    fix log name

--- experiments_llama_monitor_base.sh
+++ experiments_llama_monitor_base.sh
@@ -5,34 +5,34 @@
 PROMPT_TYPE="theseus"
 
 python execution.py meta-llama/Llama-3.2-3B-Instruct 10 theseus_same.json PAH-min0.1-h0.0 100 PAH-min0.5-h0.0.csv --prompt-type "$PROMPT_TYPE" --monitor Monitor --vllm-mode offline --vllm-url http://127.0.0.1:9996 
-echo "PAH-min0.1-h0.0 done" > log_llama.txt
+echo "PAH-min0.1-h0.0 done" > log_llama_monitor_base.txt
 python execution.py meta-llama/Llama-3.2-3B-Instruct 10 theseus_same.json PAH-min0.1-h0.25 100 PAH-min0.5-h0.25.csv --prompt-type "$PROMPT_TYPE" --monitor Monitor --vllm-mode offline --vllm-url http://127.0.0.1:9996 
-echo "PAH-min0.1-h0.25 done" > log_llama.txt
+echo "PAH-min0.1-h0.25 done" > log_llama_monitor_base.txt
 python execution.py meta-llama/Llama-3.2-3B-Instruct 10 theseus_same.json PAH-min0.1-h0.5 100 PAH-min0.5-h0.5.csv --prompt-type "$PROMPT_TYPE" --monitor Monitor --vllm-mode offline --vllm-url http://127.0.0.1:9996 
-echo "PAH-min0.1-h0.5 done" > log_llama.txt
+echo "PAH-min0.1-h0.5 done" > log_llama_monitor_base.txt
 python execution.py meta-llama/Llama-3.2-3B-Instruct 10 theseus_same.json PAH-min0.1-h0.75 100 PAH-min0.5-h0.75.csv --prompt-type "$PROMPT_TYPE" --monitor Monitor --vllm-mode offline --vllm-url http://127.0.0.1:9996 
-echo "PAH-min0.1-h0.75 done" > log_llama.txt
+echo "PAH-min0.1-h0.75 done" > log_llama_monitor_base.txt
 python execution.py meta-llama/Llama-3.2-3B-Instruct 10 theseus_same.json PAH-min0.1-h1.0 100 PAH-min0.5-h1.0.csv --prompt-type "$PROMPT_TYPE" --monitor Monitor --vllm-mode offline --vllm-url http://127.0.0.1:9996 
-echo "PAH-min0.1-h1.0 done" > log_llama.txt
+echo "PAH-min0.1-h1.0 done" > log_llama_monitor_base.txt
 
 python execution.py meta-llama/Llama-3.2-3B-Instruct 10 theseus_same.json PAH-min0.1-h0.0 100 PAH-min0.1-h0.0.csv --prompt-type "$PROMPT_TYPE" --monitor Monitor --vllm-mode offline --vllm-url http://127.0.0.1:9996 
-echo "PAH-min0.1-h0.0 done" > log_llama.txt
+echo "PAH-min0.1-h0.0 done" > log_llama_monitor_base.txt
 python execution.py meta-llama/Llama-3.2-3B-Instruct 10 theseus_same.json PAH-min0.1-h0.25 100 PAH-min0.1-h0.25.csv --prompt-type "$PROMPT_TYPE" --monitor Monitor --vllm-mode offline --vllm-url http://127.0.0.1:9996 
-echo "PAH-min0.1-h0.25 done" > log_llama.txt
+echo "PAH-min0.1-h0.25 done" > log_llama_monitor_base.txt
 python execution.py meta-llama/Llama-3.2-3B-Instruct 10 theseus_same.json PAH-min0.1-h0.5 100 PAH-min0.1-h0.5.csv --prompt-type "$PROMPT_TYPE" --monitor Monitor --vllm-mode offline --vllm-url http://127.0.0.1:9996 
-echo "PAH-min0.1-h0.5 done" > log_llama.txt
+echo "PAH-min0.1-h0.5 done" > log_llama_monitor_base.txt
 python execution.py meta-llama/Llama-3.2-3B-Instruct 10 theseus_same.json PAH-min0.1-h0.75 100 PAH-min0.1-h0.75.csv --prompt-type "$PROMPT_TYPE" --monitor Monitor --vllm-mode offline --vllm-url http://127.0.0.1:9996 
-echo "PAH-min0.1-h0.75 done" > log_llama.txt
+echo "PAH-min0.1-h0.75 done" > log_llama_monitor_base.txt
 python execution.py meta-llama/Llama-3.2-3B-Instruct 10 theseus_same.json PAH-min0.1-h1.0 100 PAH-min0.1-h1.0.csv --prompt-type "$PROMPT_TYPE" --monitor Monitor --vllm-mode offline --vllm-url http://127.0.0.1:9996 
-echo "PAH-min0.1-h1.0 done" > log_llama.txt
+echo "PAH-min0.1-h1.0 done" > log_llama_monitor_base.txt
 
 python execution.py meta-llama/Llama-3.2-3B-Instruct 10 theseus_same.json PAH-min0.3-h0.0 100 PAH-min0.3-h0.0.csv --prompt-type "$PROMPT_TYPE" --monitor Monitor --vllm-mode offline --vllm-url http://127.0.0.1:9996 
-echo "PAH-min0.3-h0.0 done" > log_llama.txt
+echo "PAH-min0.3-h0.0 done" > log_llama_monitor_base.txt
 python execution.py meta-llama/Llama-3.2-3B-Instruct 10 theseus_same.json PAH-min0.3-h0.5 100 PAH-min0.3-h0.5.csv --prompt-type "$PROMPT_TYPE" --monitor Monitor --vllm-mode offline --vllm-url http://127.0.0.1:9996 
-echo "PAH-min0.3-h0.5 done" > log_llama.txt
+echo "PAH-min0.3-h0.5 done" > log_llama_monitor_base.txt
 python execution.py meta-llama/Llama-3.2-3B-Instruct 10 theseus_same.json PAH-min0.3-h0.25 100 PAH-min0.3-h0.25.csv --prompt-type "$PROMPT_TYPE" --monitor Monitor --vllm-mode offline --vllm-url http://127.0.0.1:9996 
-echo "PAH-min0.3-h0.25 done" > log_llama.txt
+echo "PAH-min0.3-h0.25 done" > log_llama_monitor_base.txt
 python execution.py meta-llama/Llama-3.2-3B-Instruct 10 theseus_same.json PAH-min0.3-h0.75 100 PAH-min0.3-h0.75.csv --prompt-type "$PROMPT_TYPE" --monitor Monitor --vllm-mode offline --vllm-url http://127.0.0.1:9996 
-echo "PAH-min0.3-h0.75 done" > log_llama.txt
+echo "PAH-min0.3-h0.75 done" > log_llama_monitor_base.txt
 python execution.py meta-llama/Llama-3.2-3B-Instruct 10 theseus_same.json PAH-min0.3-h1.0 100 PAH-min0.3-h1.0.csv --prompt-type "$PROMPT_TYPE" --monitor Monitor --vllm-mode offline --vllm-url http://127.0.0.1:9996 
 echo "PAH-min0.3-h1.0 done" > log_llama.txt
